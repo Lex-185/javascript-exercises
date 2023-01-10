f255274bf19541be176ee49a586b600cf3084811
Lex <gravityandmadness@gmail.com>
2023-01-10 11:45:19 +0200

    Completed function to reverse a given string
    Method 1: Split, reverse and then join the reversed string.
    Method 2: Decrementing for loop.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+// 01. Split string -> reverse string -> join reverse string
+// const reverseString = function(str) {
+//     let splitStr = str.split('')
+//     let reverseArr = splitStr.reverse()
+//     let joinArr = reverseArr.join('')
+//     return joinArr
+// };
 
-};
+// 02 Decrementing for loop
+const reverseString = function(str) {
+    let reverseStr = '';
+
+    for (let i = str.length - 1; i >= 0; i--){
+        reverseStr += str[i];
+    };
+
+    return reverseStr
+}
 
 // Do not edit below this line
 module.exports = reverseString;

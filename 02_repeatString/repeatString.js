e4b7c9dd6212b04ef1d15cb29339be295c98717d
Lex <gravityandmadness@gmail.com>
2023-01-10 11:37:43 +0200

    Completed function to repeat a given string
    The first attempt was made using the repeat method.
    However, a decrementing while loop is more efficient.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,28 @@
-const repeatString = function() {
-
+// Using while loop
+const repeatString = function(str, n) {
+    let repeatString = '';
+    if (n > 0) {
+        while (n > 0){
+            repeatString += str;
+            n--;
+        }
+        return repeatString
+    } else if (n == 0 || str == '') {
+        return ''
+    } else {
+        return 'ERROR'
+    }
+    
 };
+
+// Using repeat method
+// const repeatString = function(str, n) {
+//     if (n > 0) {
+//         console.log(str.repeat(n))
+//     } else {
+//         return '';
+//     }
+// };
 
 // Do not edit below this line
 module.exports = repeatString;
